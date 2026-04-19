9449b6c9a2b2b155219d515d28106878deb6df1e
Vincent Josef V. Atienza <127913793+sCent02@users.noreply.github.com>
2026-04-19 08:53:13 +0000

    update backend bucket

--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
     backend "s3" {
-        bucket          = "cambridge-exam-terraform-backend-___"
+        bucket          = "cambridge-exam-terraform-backend-xmwdj5m1"
         key             = "exam/terraform.tfstate"
         region          = "ap-southeast-1"
         dynamodb_table  = "cambridge-exam-terraform-locks"
