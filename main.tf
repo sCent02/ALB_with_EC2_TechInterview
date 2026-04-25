a7093a5daada7d80f8ccc557a93571b98d5f5cca
Vincent Josef V. Atienza <127913793+sCent02@users.noreply.github.com>
2026-04-25 05:35:57 +0000

    modify variables

--- main.tf
+++ main.tf
@@ -1,28 +1,9 @@
-# Fetch the VPC to ensure it's valid
-data "aws_vpc" "existing" {
-  id = var.vpc_id
-}
-
-# Fetch the Subnets to ensure they are available
-data "aws_subnet" "sub1" {
-  id = var.subnet_ids[0]
-}
-
-data "aws_subnet" "sub2" {
-  id = var.subnet_ids[1]
-}
-
-# Fetch the Security Groups to ensure it's valid
-data "aws_security_group" "existing" {
-  id = var.security_group_id
-}
-
 # 1. Target Group
 resource "aws_lb_target_group" "tg" {
   name     = "exam-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = data.aws_vpc.existing.id
+  vpc_id   = var.vpc_id
 }
 
 # 2. ALB
@@ -30,8 +11,8 @@ resource "aws_lb" "alb" {
   name               = "exam-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [data.aws_security_group.existing.id]
-  subnets            = [data.aws_subnet.sub1.id, data.aws_subnet.sub2.id]
+  security_groups    = [var.security_group_id]
+  subnets            = var.subnet_ids
 }
 
 # 3. Listener
@@ -51,8 +32,8 @@ resource "aws_instance" "app_server" {
   ami                  = var.ami_id
   instance_type        = "t3.micro"
   iam_instance_profile = var.instance_profile_name
-  subnet_id            = count.index == 0 ? data.aws_subnet.sub1.id : data.aws_subnet.sub2.id
-  vpc_security_group_ids = [data.aws_security_group.existing.id]
+  subnet_id            = count.index == 0 ? var.subnet_ids[0] : var.subnet_ids[1]
+  vpc_security_group_ids = [var.security_group_id]
 
   user_data = <<-EOF
               #!/bin/bash
